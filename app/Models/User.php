6b47c20561e929062f9295e428753849f1c45dfe
Hanss <126554595+fearthans@users.noreply.github.com>
2024-04-22 12:51:18 +0000

    Update Tugas

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type'
     ];
 
     /**
@@ -41,4 +43,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected function type(): Attribute
+    {
+        return new Attribute(
+            get: fn($value) => ["user", "admin", "manager","superadmin"][$value]
+        );
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +12,55 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(isset(auth()->user()->type)){
+        if(auth()->user()->type == 'admin'){
+            $home = '/admin/home';
+        } elseif (auth()->user()->type == 'manager') {
+            $home = '/manager/home';
+        } elseif (auth()->user()->type == 'manager') {
+            $home = '/superadmin/home';
+        }
+    } else {$home = '/home';}
+    return view('welcome')->with('home', $home);
+});
+
+Auth::routes();
+
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Admin Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+    Route::get('/admin/home', [
+        HomeController::class,
+        'adminHome'
+    ])->name('admin.home');
+});
+/*------------------------------------------
+--------------------------------------------
+All Admin Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:manager'])->group(function () {
+    Route::get('/manager/home', [
+        HomeController::class,
+        'managerHome'
+    ])->name('manager.home');
+});
+
+Route::middleware(['auth', 'user-access:superadmin'])->group(function () {
+    Route::get('/superadmin/home', [
+        HomeController::class,
+        'superAdminHome'
+    ])->name('superadmin.home');
 });
